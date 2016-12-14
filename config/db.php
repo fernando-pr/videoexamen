6e655ebed428611b04ff032bb9859fad15b9cc61
Fernando Pacheco <fernando321987@gmail.com>
2016-12-14 21:09:03 +0100

    carga inicial

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=videoexamen',
+    'username' => 'videoexamen',
+    'password' => 'videoexamen',
+    'charset' => 'utf8',
+];
